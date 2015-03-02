4a5287a993427731f1ac3cca6157121a52c4899f
Semir <semko2m@gmail.com>
2015-03-02 15:41:34 +0100

    first

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa="6404 Eastern Baltimore MD 21224"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+	$telephone=4106338808;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
